3294479d6a43fbfa8df8013dd9bd250dd7d68897
Reuben Opondo <opondoreuben94@gmail.com>
2023-11-06 23:28:48 +0300

    update

--- R/gbt_data_dictionary.R
+++ R/gbt_data_dictionary.R
@@ -1,4 +1,17 @@
 
-
+#' Description of variables in `gbt_data_dictionary` dataset.
+#'
+#' A summary of the `gbt_data_dictionary`
+#'
+#' @format A tibble with 77 rows and 5 variables:
+#' \describe{
+#'   \item{data}{a vector of dataframe. That is Bonds, Allocations, Key Performance Indicators,Tranches and Projects.}
+#'   \item{ref_no}{Reference number for each variable in a dataframe.}
+#'   \item{name}{Name of each variable as it appears in their respective dataframes.}
+#'   \item{full_name}{Full name of each variable as it appears in their respective dataframes.}
+#'   \item{description}{Description of name of each variable as it appears in their respective dataframes..}
+#'
+#' }
+#' @source \url{https://www.greenbondtransparency.com/support/resources/index-gbtp-data-en-v3.xlsx}
 
 "gbt_data_dictionary"
